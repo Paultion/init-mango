161a276249d332b8e557723ddccd487573559549
paultion <Allen520>
2019-03-19 14:48:37 +0800

    fixed global command

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-'use strict';
+#!/usr/bin/env node
 
-const chalk = require('chalk');
-
-const currentNodeVersion = process.versions.node;
-const semver = currentNodeVersion.split('.');
-const major = semver[0];
-
-if (major < 4) {
-  console.error(
-    chalk.red(
-      'You are running Node ' +
-        currentNodeVersion +
-        '.\n' +
-        'Create React App requires Node 4 or higher. \n' +
-        'Please update your version of Node.'
-    )
-  );
-  process.exit(1);
-}
-
-require('./initMango');
+module.exports = require('./lib');
